1a6b52ca4582eb807f758605e07f954baebdf4f6
Ganesh Manjuluri <ganesh.edu@gmail.com>
2020-03-29 11:48:31 -0400

    Updated bucket name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-statefiles"
+    bucket  = "terraform-statefiles1"
     key     = "ec2create"
     region  = "us-east-1"
     profile = "cloud_user"
